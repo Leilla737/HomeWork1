a7687d7c3df6004da5c91b85127f6abaad4f6b53
Leilla737 <ches-lena@yandex.ru>
2022-11-24 08:43:20 +0300

    создаем репозиторий

---
+++ Program.cs
@@ -0,0 +1,119 @@
+﻿/*Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, 
+какое число большее, а какое меньшее.
+
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3*/
+
+/*Решение1*/
+/*Console.WriteLine("Input number1");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input number2");
+int numB = Convert.ToInt32(Console.ReadLine());
+
+
+if (numA > numB)
+{
+ Console.WriteLine($" {numA}");   
+}
+
+if (numA < numB);
+{
+ Console.WriteLine($"{numB}"); 
+}*/
+
+/*Решение2*/
+/*Console.WriteLine("Input number1");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input number2");
+int numB = Convert.ToInt32(Console.ReadLine());
+
+
+if (numA > numB)
+{
+ Console.WriteLine($"Max {numA}, Min {numB}");   
+}
+
+if (numA < numB);
+{
+ Console.WriteLine($"Min {numA}, Max {numB}"); 
+}*/
+
+/* Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт
+ максимальное из этих чисел.
+
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22 */
+
+/*Console.WriteLine("Input num1");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input num2");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input num3");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+int max = num1;
+
+if (num2 > max)
+
+{
+max = num2;
+}
+
+ if (num3 > max)
+{
+max = num3;
+}
+
+Console.WriteLine("Наибольшее из введённых чисел -> " + max);*/
+
+
+/*Задача 6: Напишите программу, которая на вход принимает число и выдаёт, 
+является ли число чётным (делится ли оно на два без остатка).
+
+4 -> да
+-3 -> нет
+7 -> нет*/
+
+/*Console.WriteLine("Input number1");
+int num = Convert.ToInt32(Console.ReadLine());
+
+
+if (num % 2 == 1 || num % 2 == -1)
+
+{
+Console.WriteLine("нет");
+}
+else
+{
+Console.WriteLine("да");
+}*/
+
+/*Задача 8: Напишите программу, которая на вход принимает число (N), а на 
+выходе показывает все чётные числа от 1 до N.
+
+5 -> 2, 4
+8 -> 2, 4, 6, 8*/
+
+/* Console.WriteLine("Input num");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = 1;  
+while (count < num)
+{
+if (count % 2 == 0) 
+   {
+    Console.WriteLine(count + " ");
+   }   
+    count++;  
+} */
+// 
+
+
+
+  
+        
+   
+
+           
+
